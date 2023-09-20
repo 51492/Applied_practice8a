8d116be4ed98444fd0d6248795ee8e5dd077e953
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-20 14:56:12 +0000

    [update]bookers2_アクセス制限設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def new
     @book_new = Book.new
   end
@@ -7,6 +9,7 @@ class BooksController < ApplicationController
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
     if @book_new.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book_new.id)
     else
       # ↓レンダーでindexのviewを表示する前に必要なインスタンスを用意
@@ -27,11 +30,11 @@ class BooksController < ApplicationController
     @user = @book.user
     @book_new = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -53,6 +56,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
