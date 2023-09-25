d917830d1691a9c488aa897b7912800f9d4a54a4
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-25 04:47:38 +0000

    [start]3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     flash[:notice] = "Welcome! You have signed up successfully."
-    user_path(current_user.id)
+    user_path(current_user.id) 
   end
 
   def after_sign_in_path_for(resource)
